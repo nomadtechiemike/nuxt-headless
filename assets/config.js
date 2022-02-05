ef891361037dfe5f2bce7c630d1f25a314987ca2
Mike Nomad <29424378+nomadtechiemike@users.noreply.github.com>
2022-02-05 20:49:24 +0700

    updates

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const apiEndpoint = "api.moustachedesign.xyz"; // change api.moustachedesign.xyz to your wordpress url
+const apiEndpoint = "developer6.techiemike.com"; // change api.moustachedesign.xyz to your wordpress url
 
 export default {
   appTitleShort: "Nuxt Headless",
@@ -13,11 +13,11 @@ export default {
   // these are the rest api endpoints and your wordpress url
   client: `https://${apiEndpoint}`,
   wpDomain: `https://${apiEndpoint}/wp-json`,
-  url: "https://nuxt-headless.netlify.com/", // your website url
-  loadDbName: "[starter_wp]", // db name if needed
+  url: "https://developer6.techiemike.com/", // your website url
+  loadDbName: "[techxihq_wp616]", // db name if needed
   api: {
     homePage: "/wp/v2/pages/2", // the [page_id] from WordPress
     // this url will hit an endpoint for contact form 7 plugin
-    postFormContact: "/contact-form-7/v1/contact-forms/[form_id]/feedback" // change {form_id} with the contact form 7 id provided
+    postFormContact: "/contact-form-7/v1/contact-forms/[6]/feedback" // change {form_id} with the contact form 7 id provided
   }
 };
